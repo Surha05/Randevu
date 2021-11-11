721485541f1616742c51cc91f8be82bc82d60351
kama <Kamil.kl2014@gmail.com>
2021-11-11 23:46:22 +0300

    редактировал футер

--- static/footer.php
+++ static/footer.php
@@ -1,34 +1,51 @@
 <footer class="footer">
+  <div class="container">
       <div class="footer__top-block">
-        <h1 class="footer__logo-title">Randevu</h1>
-        <p class="footer__logo-text">семейное кафе</p>
+        <ul class="footer__menu">
+          <li class="footer__menu-item footer__logo">
+            <a href="#" class="footer__menu-link">
+              <h1 class="footer__logo-title ">Randevu</h1>
+              <p class="footer__logo-text">семейное кафе</p>
+            </a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">о нас</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">оплата</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">доставка</a>
+          </li>
+          <li class="footer__menu-item">
+            <a href="#" class="footer__menu-link">контакты</a>
+          </li>
+        </ul>
+        <ul class="footer__icon">
+          <li class="footer__menu-icon">
+            <a href="#" class="footer__icon-link"><span class="icon-instagram"></span></a>
+          </li>
+          <li class="footer__menu-icon">
+            <a href="#" class="footer__icon-link"><span class="icon-WhatsApp"></span></a>
+          </li>
+          <li class="footer__menu-icon"></li>
+            <a href="#" class="footer__icon-link"><span class="icon-Telegram"></span></a>
+        </ul>
+        
       </div>
       <div class="footer__bottom-block">
-        <div class="container">
-          <ul class="footer__menu flex__jc-sb flex__ai-c">
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">о нас</a>
-            </li>
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">меню</a>
-            </li>
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">оплата</a>
-            </li>
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">доставка</a>
-            </li>
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">контакты</a>
-            </li>
-            <li class="footer__menu-item">
-              <a href="#" class="footer__menu-link">авторизация</a>
-            </li>
+        <div class="footer__bottom-left">
+          <h3 class="footer__subtitle"></h3>
+          <ul class="footer__sub-menu">
+            
           </ul>
         </div>
+        <div class="footer__bottom-right"></div>
+
         <div class="copyrightText-block">
           <p class="copyrightText">Сайт создан на SURHA.ru</p>
           <p class="copyrightText">Все права защищенны. Randevu. 2021</p>
         </div>
       </div>
-    </footer>
+    </div>
+</footer>
