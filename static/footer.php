770ce430bbdd644b4e2cf8b7affbe5fdb690f333
kama <Kamil.kl2014@gmail.com>
2021-11-14 11:55:31 +0300

    редактировал футер

--- static/footer.php
+++ static/footer.php
@@ -1,27 +1,27 @@
 <footer class="footer">
-  <div class="container">
-      <div class="footer__top-block">
-        <ul class="footer__menu">
-          <li class="footer__menu-item footer__logo">
-            <a href="#" class="footer__menu-link">
-              <h1 class="footer__logo-title ">Randevu</h1>
-              <p class="footer__logo-text">семейное кафе</p>
-            </a>
-          </li>
-          <li class="footer__menu-item">
-            <a href="#" class="footer__menu-link">о нас</a>
-          </li>
-          <li class="footer__menu-item">
-            <a href="#" class="footer__menu-link">оплата</a>
-          </li>
-          <li class="footer__menu-item">
-            <a href="#" class="footer__menu-link">доставка</a>
-          </li>
-          <li class="footer__menu-item">
-            <a href="#" class="footer__menu-link">контакты</a>
-          </li>
-        </ul>
-        <ul class="footer__icon">
+  <div class="footer__wrapper-top">
+    <div class="footer__top-block">
+      <ul class="footer__menu">
+        <li class="footer__menu-item footer__logo">
+          <a href="#" class="footer__menu-link">
+            <h1 class="footer__logo-title">Randevu</h1>
+            <p class="footer__logo-text">семейное кафе</p>
+          </a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="#" class="footer__menu-link">о нас</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="#" class="footer__menu-link">оплата</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="#" class="footer__menu-link">доставка</a>
+        </li>
+        <li class="footer__menu-item">
+          <a href="#" class="footer__menu-link">контакты</a>
+        </li>
+      </ul>
+      <ul class="footer__icon">
           <li class="footer__menu-icon">
             <a href="#" class="footer__icon-link"><span class="icon-instagram"></span></a>
           </li>
@@ -31,21 +31,64 @@
           <li class="footer__menu-icon"></li>
             <a href="#" class="footer__icon-link"><span class="icon-Telegram"></span></a>
         </ul>
-        
-      </div>
-      <div class="footer__bottom-block">
-        <div class="footer__bottom-left">
-          <h3 class="footer__subtitle"></h3>
-          <ul class="footer__sub-menu">
-            
-          </ul>
         </div>
-        <div class="footer__bottom-right"></div>
-
-        <div class="copyrightText-block">
-          <p class="copyrightText">Сайт создан на SURHA.ru</p>
-          <p class="copyrightText">Все права защищенны. Randevu. 2021</p>
-        </div>
-      </div>
     </div>
+  </div>
+  <div class="footer__wrapper-bottom">
+    <div class="footer__bottom-block">
+      <div class="footer__bottom-left">
+        <h4 class="footer__subtitle">Меню</h4>
+        <ul class="footer__sub-menu">
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">пицца</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">шашлыки</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">супы</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">завтраки</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">гарниры</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">мясные</a></li> 
+        </ul>
+        <ul class="footer__sub-menu">
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">роллы</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">салаты</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">чуду и мучное</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">десерты</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">чаи</a></li>
+          <li class="footer__sub-item"><a href="#" class="footer__sub__link">напитки</a></li>
+        </ul>
+      </div>
+      <div class="footer__bottom-right">
+        <div class="wrapper__footer-contact">
+          <div class="footer__address">
+            <h4 class="footer__contact-h4">Адрес</h4>
+            <p class="footer__contact-text">г. Дербент</p>
+            <p class="footer__contact-text">ул. Гагарина, 10</p>
+         </div>
+         <div class="working-hours__block">
+            <h4 class="working-hours__h4">Время работы</h4>
+            <p class="workingh-hours__text">с 9:00 до 21:30</p>
+          </div>
+        </div>
+        <div class="wrapper__footer-contact">
+          <div class="footer__contact">
+            <h4 class="footer__contact-h4">Телефоны</h4>
+            <p class="footer__contact-text">
+              <a href="#" class="footer__tell-link">+7(964) 007-07-07</a>
+            </p>
+            <p class="footer__contact-text">
+              <a href="#" class="footer__tell-link">+777-005</a>
+            </p>
+          </div>
+          <div class="copyright">
+            <p class="copyright__title">Randevu. 2021</p>
+            <p class="copyrightText">Все права защищенны</p>
+            <p class="copyrightText copy">Сайт создан на
+              <a href="#" class="copyrightText__link">SURHA.ru</a>
+            </p>
+          </div>
+        </div>
+      </div> 
+      <p class="footer__copyright">Сайт создан на
+        <a href="#" class="footer__copyright-link">SURHA.ru</a>
+      </p>
+    </div>
+  </div>
 </footer>
