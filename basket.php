b90d0cc7a3c1de8abf915ffd71a7e67c27091bd2
kama <Kamil.kl2014@gmail.com>
2021-11-30 21:38:59 +0300

    редактировал форму в корзине

--- basket.php
+++ basket.php
@@ -125,7 +125,7 @@
           </div>
           <div class="form-wrapper"> 
             <h2 class="form__title">Данные покупателя</h2>
-            <div class="form">
+            <div class="form form-buer">
               <form action="#" id="form" class="form__body">
                 <div class="form__box-top">
                   <div class="form__item">
@@ -145,7 +145,57 @@
                 </div> 
               </form>
             </div>
-            <h2 class="form__title form-title-delivery">Cпособ доставки</h2>
+            <h2 class="form__title form__title-delivery">Cпособ доставки</h2>
+            <div class="form form-delivery">
+              <div class="form-control__pickup">
+                <label class="form-control__label">
+                  <input class="form__real-radio" type="radio"                   name="delivery"/>
+                  Самовывоз
+                  <span class="form__gustom-radio"></span>
+                </label>
+                <p class="form-control__text">Самовывоз осуществляется по адресу: г. Дербент ул. Гагарина 10</p>
+              </div>
+              <div class="form-control__delivery">
+                <label class="form-control__label">
+                  <input class="form__real-radio" type="radio"                   name="delivery"/>                  Доставка
+                  <span class="form__gustom-radio"></span>
+                </label>
+                <div class="form__map">
+                  <span class="icon-map"></span>
+                  <p class="form__map-text">Дербент</p>
+                  <span class="icon-Union"></span>
+                </div>
+                <form action="#" class="form__body-delivery">
+                  <div class="form-control__item">
+						        <label for="formStreet" class="form__label-item">Улица<span>*</span></label>
+						        <input id="formStreet" type="text" placeholder="Улица"  name="street" class="form__input-item">
+					        </div>
+                  <div class="form-control__item">
+						        <label for="formStreet" class="form__label-item">Комментарий</label>
+						        <input id="formCommentary" type="text" placeholder="Комментарий"  name="commentary" class="form__input-item form__input-comment">
+					        </div>
+                </form>
+              </div>
+            </div>
+            <h2 class="form__title form__title-pay">Cпособ оплаты</h2>
+            <div class="form form-pay">
+              <div class="form-control__pay-online">
+                <label class="form-control__label">
+                  <input class="form__real-radio" type="radio"                   name="pay"/>
+                  Оплата при получении
+                  <span class="form__gustom-radio"></span>
+                </label>
+                <p class="form-control__text">Оплата наличными и картой при получении заказа.</p>
+              </div>
+              <div class="form-control__pay-cart">
+                <label class="form-control__label">
+                  <input class="form__real-radio" type="radio"                   name="pay"/>
+                  Онлайн-оплата картой
+                  <span class="form__gustom-radio"></span>
+                </label>
+                <p class="form-control__text">Онлайн-оплата на сайте банковской картой Visa, MasterCard или Мир (без комиссии).</p>
+              </div>
+            </div>
           </div>
         </div>
         <div class="basket-container__right">
@@ -176,6 +226,37 @@
             </ul>
            <button class="basket__order-btn">Оформить заказ</button>
            <p class="basket__order-consent">Нажимая на кнопку, я даю свое согласие на обработку персональных данных</p>
+          </div>
+        </div>
+        <div class="basket-container__bottom">
+          <div class="basket__order">
+            <h2 class="basket__order-title">Ваш заказ
+            </h2>
+            <ul class="basket__order-items">
+              <li class="basket__order-item">
+                <p class="basket__order-name">Наименований</p>
+                <p class="basket__order-price">4 Р</p>
+              </li>
+              <li class="basket__order-item">
+                <p class="basket__order-cost">Стоимость</p>
+                <p class="basket__order-price">1940 Р</p>
+              </li>
+              <li class="basket__order-item">
+                <p class="basket__order-delivery">Доставка</p>
+                <p class="basket__order-price">100 Р</p>
+              </li>
+              <li class="basket__order-item">
+                <p class="basket__order-reduction">Скидка</p>
+                <p class="basket__order-price">0 Р</p>
+              </li>
+              <li class="basket__order-item">
+                <p class="basket__order-all">Итого</p>
+                <p class="basket__order-price">1940 Р</p>
+              </li>
+            </ul>
+           <button class="basket__order-btn">Оформить заказ</button>
+           <p class="basket__order-consent">Нажимая на кнопку, я даю свое согласие на обработку персональных данных</p>
+          </div>
         </div>
       </div>
     </div>
