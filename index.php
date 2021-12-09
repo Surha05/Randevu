77eb4a1ea09b2d8160aec2347d8e1e3406ab93c2
Surha <surha_dag@mail.ru>
2021-12-09 19:28:35 +0300

    add head.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head> 
-    <meta charset="UTF-8" /> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name=”robots” content=”noindex, nofollow”/>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="icon.css" />
-    <title>Document</title>
-  </head>
+  <?php include_once '/modules/head/head.php'; ?>
   <body> 
     <header class="header">
       <?php
---
+++ modules/head/head.php
@@ -0,0 +1,10 @@
+<head> 
+    <meta charset="UTF-8" /> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name=”robots” content=”noindex, nofollow”/>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="icon.css" />
+    <title>Document</title>
+</head>
