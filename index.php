c3c942b470a891c0a39e1623ec3d7bce6ee5b09c
Surha <surha_dag@mail.ru>
2021-12-09 19:31:30 +0300

    add header module

--- index.php
+++ index.php
@@ -2,23 +2,7 @@
 <html lang="ru">
   <?php include_once '/modules/head/head.php'; ?>
   <body> 
-    <header class="header">
-      <?php
-      include "static/menu.php";
-      ?>
-      <div class="container flex__jc-c flex__ai-c">
-        <div class="rectangle-top"></div>
-        <h2 class="banner-h2">
-          окунись <br />
-          <span>в мир вкуса</span>
-        </h2>
-        <div class="rectangle-bottom"></div>
-        <h3 class="banner-h3">
-          Добро пожаловать <br />
-          <span>в Рандеву</span>
-        </h3>
-      </div>
-    </header>
+    <?php include '/modules/header/header.php'; ?>
     <section class="about">
       <div class="container flex__jc-sb">
         <div class="about__left-block">
