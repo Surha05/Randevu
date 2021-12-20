eb01ed50ad58aaf7f0c497265659c1f25707d58f
Surha <surha_dag@mail.ru>
2021-12-20 19:12:07 +0300

    add delight.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ru">
   <?php include_once 'modules/head/head.php'; ?>
   <body> 
-    <?php include 'modules/header/header.php'; ?>
+    <?php include_once 'modules/header/header.php'; ?>
     <?php include 'modules/about/about.php'; ?>
     <?php include 'modules/menu-cart/menu-cart.php'; ?>
     <?php include 'modules/working-hours/working-hours.php'; ?>
@@ -10,28 +10,8 @@
     <?php include 'modules/banner/banner.php'; ?>
     <?php include 'modules/dishes-menu/dishes-menu.php'; ?>
     <?php include 'modules/banner2/banner2.php'; ?>
-    <section class="delight">
-      <div class="delight__left-block">
-        <figure class="delight__box-img">
-          <img src="img/delight/Rectangle 46.png" alt="" class="delight__img" />
-        </figure>
-        <figure class="delight__box-img">
-          <img src="img/delight/Rectangle 47.png" alt="" class="delight__img" />
-        </figure>
-      </div>
-      <div class="delight__right-block">
-        <h2 class="delight__title">ВОСТОРГ</h2>
-        <p class="delight__text">
-          Мы обещаем интимную и непринужденную обстановку ужина, которая
-          предлагает что-то особенное для местных и иностранных посетителей и
-          гарантирует, что вы каждый раз будете получать незабываемые
-          впечатления от еды.
-        </p>
-      </div>
-    </section>
-    <?php
-    include "static/footer.php";
-    ?>
+    <?php include 'modules/delight/delight.php'; ?>
+    <?php include_once "modules/footer/footer.php"; ?>
     <!-- Подключаем тогл -->
     <script src="js/toggle.js"></script>
   </body>
---
+++ modules/delight/delight.php
@@ -0,0 +1,19 @@
+<section class="delight">
+      <div class="delight__left-block">
+        <figure class="delight__box-img">
+          <img src="img/delight/Rectangle 46.png" alt="" class="delight__img" />
+        </figure>
+        <figure class="delight__box-img">
+          <img src="img/delight/Rectangle 47.png" alt="" class="delight__img" />
+        </figure>
+      </div>
+      <div class="delight__right-block">
+        <h2 class="delight__title">ВОСТОРГ</h2>
+        <p class="delight__text">
+          Мы обещаем интимную и непринужденную обстановку ужина, которая
+          предлагает что-то особенное для местных и иностранных посетителей и
+          гарантирует, что вы каждый раз будете получать незабываемые
+          впечатления от еды.
+        </p>
+      </div>
+    </section>
