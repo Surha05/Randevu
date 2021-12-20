247d4f335a882ecbae22319148f251236b2fb9df
Surha <surha_dag@mail.ru>
2021-12-20 19:07:55 +0300

    add dishes-menu.php

---
+++ modules/dishes-menu/dishes-menu.php
@@ -0,0 +1,42 @@
+<section class="dishes-menu">
+      <div class="dishes-menu__left-block">
+        <h2 class="dishes-menu__title">МЕНЮ</h2>
+        <p class="dishes-menu__icon">*</p>
+        <p class="dishes-menu__text">
+          Для тех, кто ценит истинное наслаждение едой приходите по соседству и
+          насыщайте свои желания нашими постоянно меняющимися небольшими
+          тарелками, вдохновленными интернациональными и сезонными блюдами. Мы
+          любим готовить много разной еды для вас.
+        </p>
+      </div>
+      <div class="dishes-menu__right-block">
+        <figure class="dishes-menu__box-img">
+          <img
+            src="img/dishes-menu/Rectangle 39.png"
+            alt=""
+            class="dishes-menu__img"
+          />
+        </figure>
+        <figure class="dishes-menu__box-img">
+          <img
+            src="img/dishes-menu/Rectangle 42.png"
+            alt=""
+            class="dishes-menu__img"
+          />
+        </figure>
+        <figure class="dishes-menu__box-img">
+          <img
+            src="img/dishes-menu/Rectangle 41.png"
+            alt=""
+            class="dishes-menu__img"
+          />
+        </figure>
+        <figure class="dishes-menu__box-img">
+          <img
+            src="img/dishes-menu/Rectangle 43.png"
+            alt=""
+            class="dishes-menu__img"
+          />
+        </figure>
+      </div>
+    </section>
