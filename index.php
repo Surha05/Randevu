e27576a4bcc548ab2ac6ace25ab963a33242699d
Surha <surha_dag@mail.ru>
2021-12-20 19:05:24 +0300

    add our-history.php

--- index.php
+++ index.php
@@ -6,24 +6,7 @@
     <?php include 'modules/about/about.php'; ?>
     <?php include 'modules/menu-cart/menu-cart.php'; ?>
     <?php include 'modules/working-hours/working-hours.php'; ?>
-    <section class="our-history">
-      <div class="our-history__left-block">
-        <figure class="our-history__box-img">
-          <img
-            src="img/our-history/Rectangle 37.png"
-            alt=""
-            class="our-history__img"
-          />
-        </figure>
-      </div>
-      <div class="our-history__right-block">
-        <h2 class="our-history__title">НАША ИСТОРИЯ</h2>
-        <p class="our-history__text">
-          Рандеву - это ресторан, бар, и кофейня, расположенная на оживленной
-          улице Дербента. С застекленным фасадом здания, с прекрасным видом.
-        </p>
-      </div>
-    </section>
+    <?php include 'modules/our-history/our-history.php'; ?>
     <section class="banner">
       <figure class="banner__box-img">
         <img src="img/banner/Rectangle 36.png" alt="" class="banner__img" />
---
+++ modules/our-history/our-history.php
@@ -0,0 +1,18 @@
+<section class="our-history">
+      <div class="our-history__left-block">
+        <figure class="our-history__box-img">
+          <img
+            src="img/our-history/Rectangle 37.png"
+            alt=""
+            class="our-history__img"
+          />
+        </figure>
+      </div>
+      <div class="our-history__right-block">
+        <h2 class="our-history__title">НАША ИСТОРИЯ</h2>
+        <p class="our-history__text">
+          Рандеву - это ресторан, бар, и кофейня, расположенная на оживленной
+          улице Дербента. С застекленным фасадом здания, с прекрасным видом.
+        </p>
+      </div>
+    </section>
