a8ba4c681de898cd64ae0aa5b79db6830cf8c26c
Surha <surha_dag@mail.ru>
2021-12-20 19:03:58 +0300

    add working-hours.php

--- index.php
+++ index.php
@@ -5,25 +5,7 @@
     <?php include 'modules/header/header.php'; ?>
     <?php include 'modules/about/about.php'; ?>
     <?php include 'modules/menu-cart/menu-cart.php'; ?>
-    <section class="working-hours">
-      <h2 class="working-hours__title">Время работы</h2>
-      <figure class="working-hours__img-box">
-        <img
-          src="img/working-hours/столик.png"
-          alt=""
-          class="working-hours__img"
-        />
-      </figure>
-      <p class="working-hours__line"></p>
-      <figure class="working-hours__img-box">
-        <img
-          src="img/working-hours/часы.png"
-          alt=""
-          class="working-hours__img"
-        />
-      </figure>
-      <p class="working-hours__text">c 9:00 до 21:30 ежедневно</p>
-    </section>
+    <?php include 'modules/working-hours/working-hours.php'; ?>
     <section class="our-history">
       <div class="our-history__left-block">
         <figure class="our-history__box-img">
---
+++ modules/working-hours/working-hours.php
@@ -0,0 +1,19 @@
+		<section class="working-hours">
+      <h2 class="working-hours__title">Время работы</h2>
+      <figure class="working-hours__img-box">
+        <img
+          src="img/working-hours/столик.png"
+          alt=""
+          class="working-hours__img"
+        />
+      </figure>
+      <p class="working-hours__line"></p>
+      <figure class="working-hours__img-box">
+        <img
+          src="img/working-hours/часы.png"
+          alt=""
+          class="working-hours__img"
+        />
+      </figure>
+      <p class="working-hours__text">c 9:00 до 21:30 ежедневно</p>
+    </section>
