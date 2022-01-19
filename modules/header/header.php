096372a940571d22fe83e51a4dae590183a3b13f
Surha <surha_dag@mail.ru>
2022-01-19 15:09:39 +0300

    fix .container and logo

--- modules/header/header.php
+++ modules/header/header.php
@@ -1,15 +1,9 @@
 <header class="header">
-      <?php include 'modules/menu/menu.php'; ?>
-      <div class="container flex__jc-c flex__ai-c">
-        <div class="rectangle-top"></div>
-        <h2 class="banner-h2">
-          окунись <br />
-          <span>в мир вкуса</span>
-        </h2>
-        <div class="rectangle-bottom"></div>
-        <h3 class="banner-h3">
-          Добро пожаловать <br />
-          <span>в Рандеву</span>
-        </h3>
-      </div>
+  <?php include 'modules/menu/menu.php'; ?>
+  <div class="container flex__jc-c flex__ai-c">
+    <div class="rectangle-top"></div>
+    <h2 class="banner-h2">окунись <br /><span>в мир вкуса</span></h2>
+    <div class="rectangle-bottom"></div>
+    <h3 class="banner-h3">Добро пожаловать <br /><span>в Рандеву</span></h3>
+  </div>
 </header>
