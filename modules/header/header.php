43fd05b6d9dedbb10e60a65203df0f111dfd0540
Surha <surha_dag@mail.ru>
2022-01-20 16:45:01 +0300

    fix header

--- modules/header/header.php
+++ modules/header/header.php
@@ -1,9 +1,10 @@
 <header class="header">
   <?php include 'modules/menu/menu.php'; ?>
-  <div class="container flex__jc-c flex__ai-c">
-    <div class="rectangle-top"></div>
-    <h2 class="banner-h2">окунись <br /><span>в мир вкуса</span></h2>
-    <div class="rectangle-bottom"></div>
-    <h3 class="banner-h3">Добро пожаловать <br /><span>в Рандеву</span></h3>
+  <div class="container flex__jc-c flex__ai-c header__bottom-block">
+    <div class="plea">
+      <div class="rectangle-top"></div>
+      <h2 class="banner-h2">окунись <br /><span>в мир вкуса</span></h2>
+      <div class="rectangle-bottom"></div>
+    </div>
   </div>
 </header>
