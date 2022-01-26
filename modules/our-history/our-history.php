cc1f8d96f5f72d0ff9c4c1c34307a86fb7bafce0
Surha <surha_dag@mail.ru>
2022-01-26 19:10:54 +0300

    fix our history section

--- modules/our-history/our-history.php
+++ modules/our-history/our-history.php
@@ -1,5 +1,5 @@
 <section class="our-history">
-      <div class="our-history__left-block">
+      <div class="our-history__left-block" data-aos="fade-right">
         <figure class="our-history__box-img">
           <img
             src="img/our-history/Rectangle 37.png"
@@ -8,7 +8,7 @@
           />
         </figure>
       </div>
-      <div class="our-history__right-block">
+      <div class="our-history__right-block" data-aos="fade-left">
         <h2 class="our-history__title">НАША ИСТОРИЯ</h2>
         <p class="our-history__text">
           Рандеву - это ресторан, бар, и кофейня, расположенная на оживленной
