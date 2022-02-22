6229d9344d696984e9dbae3bbe00696b623ffa1b
Surha <surha_dag@mail.ru>
2022-02-22 16:52:48 +0300

    add font-awesome

--- modules/head/head.php
+++ modules/head/head.php
@@ -4,8 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name=”robots” content=”noindex, nofollow”/>
     <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/fonts/font-awesome/css/all.min.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="icon.css" />
-    <title>Document</title>
+    <title>Семейное кафе Рандеву</title>
 </head>
--- modules/menu-cart/menu-cart.php
+++ modules/menu-cart/menu-cart.php
@@ -1,167 +1,119 @@
-    <section class="menu-cart">
-      <h2 class="menu-cart__title">МЕНЮ</h2>
-      <div class="container">
-        <div class="menu-cart__wrapper">
-          <div class="menu-cart__wrapper-top"></div>
-          <div class="menu-cart__wrapper-bottom"></div>
-          <ul class="menu-cart__container">
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ПИЦЦА</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ШАШЛЫКИ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">СУПЫ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ЗАВТРАКИ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ГАРНИРЫ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">МЯСНЫЕ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">РОЛЛЫ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">САЛАТЫ</p>
-              </a>
-              <p class="menu-card__line"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ЧУДУ И МУЧНОЕ</p>
-              </a>
-              <p class="menu-card__line menu-card__line-chudu"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ДЕСЕРТЫ</p>
-              </a>
-              <p class="menu-card__line menu-card__line-desert"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">НАПИТКИ</p>
-              </a>
-              <p class="menu-card__line menu-card__line-drink"></p>
-            </li>
-            <li class="menu-cart__item">
-              <a href="/pages/breeding.php" class="menu-cart__link">
-                <figure class="menu-cart__img-box">
-                  <img
-                    src="img/меню/menu-img.png"
-                    alt=""
-                    class="menu-card__img"
-                  />
-                </figure>
-                <p class="menu-card__name">ЧАЙ</p>
-              </a>
-              <p class="menu-card__line menu-card__line-tea"></p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
+<section class="menu-cart">
+  <h2 class="menu-cart__title">МЕНЮ</h2>
+  <div class="">
+    <div class="menu-cart__wrapper container">
+      <div class="menu-cart__wrapper-top"></div>
+      <div class="menu-cart__wrapper-bottom"></div>
+      <ul class="menu-cart__container">
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ПИЦЦА</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ШАШЛЫКИ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">СУПЫ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ЗАВТРАКИ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ГАРНИРЫ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">МЯСНЫЕ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">РОЛЛЫ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">САЛАТЫ</p>
+          </a>
+          <p class="menu-card__line"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ЧУДУ И МУЧНОЕ</p>
+          </a>
+          <p class="menu-card__line menu-card__line-chudu"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ДЕСЕРТЫ</p>
+          </a>
+          <p class="menu-card__line menu-card__line-desert"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">НАПИТКИ</p>
+          </a>
+          <p class="menu-card__line menu-card__line-drink"></p>
+        </li>
+        <li class="menu-cart__item">
+          <a href="/pages/breeding.php" class="menu-cart__link">
+            <figure class="menu-cart__img-box">
+              <img src="img/меню/menu-img.png" alt="" class="menu-card__img" />
+            </figure>
+            <p class="menu-card__name">ЧАЙ</p>
+          </a>
+          <p class="menu-card__line menu-card__line-tea"></p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
